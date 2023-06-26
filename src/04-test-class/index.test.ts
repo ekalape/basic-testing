53812343c66a076a4deba8dcdcadecc4596336bb
Ekalape <ekalape@outlook.it>
2023-06-26 19:31:31 +0200

    feat: class tests task completed, also updated author's fixes from source

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,64 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount, BankAccount, InsufficientFundsError, TransferFailedError, SynchronizationFailedError } from './index';
 
 describe('BankAccount', () => {
+  const newBankAcc = getBankAccount(1000);
+  const otherBankAcc = getBankAccount(0)
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(newBankAcc).toBeInstanceOf(BankAccount);
+    expect(newBankAcc.getBalance()).toBe(1000)
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => newBankAcc.withdraw(2000)).toThrow(InsufficientFundsError);
   });
 
   test('should throw TransferFailedError error when transferring more than balance', () => {
-    // Write your test here
+    expect(() => newBankAcc.transfer(2000, otherBankAcc)).toThrow(InsufficientFundsError);
   });
 
-  test('should throw error when transferring to the same account', () => {
-    // Write your test here
+  test('should throw TransferFailedError when transferring to the same account', () => {
+    expect(() => newBankAcc.transfer(500, newBankAcc)).toThrow(TransferFailedError);
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const depMoney = jest.spyOn(newBankAcc, "deposit");
+    newBankAcc.deposit(1000);
+    expect(depMoney).toBeCalledTimes(1);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const witMoney = jest.spyOn(newBankAcc, "withdraw");
+    newBankAcc.withdraw(1000);
+    expect(witMoney).toBeCalledTimes(1);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const transMoney = jest.spyOn(newBankAcc, "transfer");
+    newBankAcc.transfer(500, otherBankAcc);
+    expect(transMoney).toBeCalledTimes(1);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    jest.spyOn(otherBankAcc, 'fetchBalance').mockReturnValue(new Promise(res => res(2000)));
+    const balance = await otherBankAcc.fetchBalance();
+    expect(balance).toEqual(expect.any(Number))
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    jest.spyOn(otherBankAcc, 'fetchBalance').mockReturnValue(new Promise(res => res(200)));
+    await otherBankAcc.synchronizeBalance();
+    expect(otherBankAcc.getBalance()).toEqual(200)
+
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    jest.spyOn(otherBankAcc, 'fetchBalance').mockReturnValue(new Promise(res => res(null)));
+    expect(async () => await otherBankAcc.synchronizeBalance()).rejects.toThrow(SynchronizationFailedError)
   });
 });
